11622b795b84060799d5e9db94372e1b87eb8d58
William McCullough <ZahFoxTales@gmail.com>
2018-07-31 23:58:44 -0500

    Modified the ChooseNextWaypoint class to find it's Index during execution from the Blackboard Component.

--- Source/TestingGrounds/Private/TP_ThirdPerson/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/TP_ThirdPerson/ChooseNextWaypoint.cpp
@@ -1,11 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"))
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	return EBTNodeResult::Succeeded;
 }
 
